d7e087aa6dbd7cad56f24ff3b4a462ebde279fe6
Dash <daneshaurus@gmail.com>
2025-04-07 12:50:02 +0800

    Penambahan sahamList, addSaham, removeSaham, dan viewInvestmentProducts

--- src/Admin.java
+++ src/Admin.java
@@ -4,23 +4,54 @@ import java.util.*;
 
 public class Admin {
     private List<SBN> sbnList;
+    private List<Saham> sahamList;
 
     public Admin() {
         this.sbnList = new ArrayList<>();
     }
 
+    // SBN
     public void addSBN(SBN sbn) {
         sbnList.add(sbn);
-        System.out.println("src.SBN " + sbn.getName() + " berhasil ditambahkan!");
+        System.out.println("SBN " + sbn.getName() + " berhasil ditambahkan!");
     }
 
     public void removeSBN(String name) {
         sbnList.removeIf(sbn -> sbn.getName().equals(name));
-        System.out.println("src.SBN " + name  + "berhasil dihapus.");
+        System.out.println("SBN " + name  + "berhasil dihapus.");
     }
 
     public List<SBN> getSBNList() {
         return sbnList;
+    }
+
+    // Saham
+    public void addSaham(Saham saham) {
+        sahamList.add(saham);
+        System.out.println("Saham added: " + saham.getCompanyName());
+    }
+
+    public void removeSaham(String kode){
+        sahamList.removeIf(saham -> saham.getCode().equals(kode));
+    }
+
+    public List<Saham> getSahamList() {
+        return sahamList;
+    }
+
+    // View Product List
+    public void viewInvestmentProducts() {
+        // List Produk SBN
+        System.out.println("SBN List:");
+        for (SBN sbn : sbnList) {
+            System.out.println(sbn);
+        }
+
+        // List Produk Saham
+        System.out.println("\nStock List:");
+        for (Saham saham : sahamList) {
+            System.out.println(saham);
+        }
     }
 
 }
