4a9d6c8b8ebd199b414118596bc16818bf30375c
Dash <daneshaurus@gmail.com>
2025-04-07 12:51:15 +0800

    Penambahan Class Customer

---
+++ src/Customer.java
@@ -0,0 +1,24 @@
+package src;
+
+public class Customer {
+    // melakukan investasi dan melihat porto
+    private Portofolio portofolio;
+
+    public Customer() {
+        this.portofolio = new Portofolio();
+    }
+
+    public void investSaham(Saham saham, int quantity) {
+        portofolio.addSaham(saham, quantity);
+        System.out.println("Invested in stock: " + saham.getCompanyName());
+    }
+
+    public void investSBN(SBN sbn, double amount) {
+        portofolio.addSBN(sbn, amount);
+        System.out.println("Invested in SBN: " + sbn.getName());
+    }
+    public void viewPortfolio() {
+        portofolio.viewPortofolio();
+    }
+
+}
