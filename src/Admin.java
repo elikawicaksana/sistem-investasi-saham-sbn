f6a3453824141700fbbaeba18d7b30c94489d62f
Dash <daneshaurus@gmail.com>
2025-04-15 13:46:09 +0800

    Mengubah format tampilan dan beberapa variabel ke bahasa indonesia

--- src/Admin.java
+++ src/Admin.java
@@ -2,62 +2,63 @@ package src;
 
 import java.util.*;
 
-public class Admin {
-    private List<SBN> sbnList;
-    private List<Saham> sahamList;
+public class Admin extends User {
+    private List<SBN> listSBN;
+    private List<Saham> listSaham;
 
     public Admin(String username, String password) {
         super(username, password);
-        this.daftarSaham = new ArrayList<>();
-        this.daftarSBN = new ArrayList<>();
+        this.listSaham = new ArrayList<>();
+        this.listSBN = new ArrayList<>();
     }
 
-    @Override
+    //  Menampilkan dan menjalankan menu admin setelah admin berhasil login.
+    @Override // Override method dari superclass User
     public void showMenu() {
-        AdminMenu adminMenu = new AdminMenu(daftarSaham, daftarSBN);
+        AdminMenu adminMenu = new AdminMenu();
         adminMenu.viewMenu();
     }
 
     // SBN
     public void addSBN(SBN sbn) {
-        sbnList.add(sbn);
+        listSBN.add(sbn);
         System.out.println("SBN " + sbn.getName() + " berhasil ditambahkan!");
     }
 
     public void removeSBN(String name) {
-        sbnList.removeIf(sbn -> sbn.getName().equals(name));
+        listSBN.removeIf(sbn -> sbn.getName().equals(name));
         System.out.println("SBN " + name  + "berhasil dihapus.");
     }
 
     public List<SBN> getSBNList() {
-        return sbnList;
+        return listSBN;
     }
 
     // Saham
     public void addSaham(Saham saham) {
-        sahamList.add(saham);
+        listSaham.add(saham);
         System.out.println("Saham added: " + saham.getCompanyName());
     }
 
     public void removeSaham(String kode){
-        sahamList.removeIf(saham -> saham.getCode().equals(kode));
+        listSaham.removeIf(saham -> saham.getCode().equals(kode));
     }
 
     public List<Saham> getSahamList() {
-        return sahamList;
+        return listSaham;
     }
 
     // View Product List
     public void viewInvestmentProducts() {
         // List Produk SBN
         System.out.println("SBN List:");
-        for (SBN sbn : sbnList) {
+        for (SBN sbn : listSBN) {
             System.out.println(sbn);
         }
 
         // List Produk Saham
         System.out.println("\nStock List:");
-        for (Saham saham : sahamList) {
+        for (Saham saham : listSaham) {
             System.out.println(saham);
         }
     }
