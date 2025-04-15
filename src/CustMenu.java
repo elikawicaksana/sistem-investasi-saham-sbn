0fca3c23bc8ae03ea1df6cecbdebeb08947f3b68
Dash <daneshaurus@gmail.com>
2025-04-15 13:47:47 +0800

    Mengubah format tampilan, menambahkan dummy user, case keluar menjadi opsi 4, dan beberapa variabel ke bahasa inggris

--- src/CustMenu.java
+++ src/CustMenu.java
@@ -3,46 +3,47 @@ package src;
 import java.util.*;
 
 public class CustMenu {
-    private List<Saham> daftarSaham;
-    private List<SBN> daftarSBN;
+    private List<Saham> listSaham;
+    private List<SBN> listSBN;
     private Customer customer;
     private Scanner scanner;
 
     public CustMenu(Customer customer) {
         this.customer = customer;
-        daftarSaham = new ArrayList<>();
-        daftarSBN = new ArrayList<>();
+        listSaham = new ArrayList<>();
+        listSBN = new ArrayList<>();
         scanner = new Scanner(System.in);
     }
 
     public void viewMenu() {
         while (true) {
-            System.out.println("\n--- Menu Customer ---");
-            System.out.println("1. Beli Saham");
-            System.out.println("2. Jual Saham");
-            System.out.println("3. Beli SBN");
-            System.out.println("4. Simulasi SBN");
-            System.out.println("5. Lihat Portofolio");
-            System.out.println("6. Logout");
+            System.out.println("===== Menu Customer =====");
+            System.out.println("  1. Beli Saham");
+            System.out.println("  2. Jual Saham");
+            System.out.println("  3. Beli SBN");
+            System.out.println("  4. Simulasi SBN");
+            System.out.println("  5. Lihat Portofolio");
+            System.out.println("  6. Keluar");
+            System.out.println("=========================");
             System.out.print("Pilih Menu (1-6): ");
             int choice = scanner.nextInt();
             scanner.nextLine(); // Buang enter
 
             switch (choice) {
                 case 1:
-                    buySaham();
+                    // buySaham();
                     break;
                 case 2:
-                    sellSaham();
+                    // sellSaham();
                     break;
                 case 3:
-                    buySBN();
+                    // buySBN();
                     break;
                 case 4:
-                    simulationSBN();
+                    // simulationSBN();
                     break;
                 case 5:
-                    viewPortfolio();
+                    // viewPortfolio();
                     break;
                 case 6:
                     System.out.println("Keluar dari sistem customer.");
@@ -54,7 +55,7 @@ public class CustMenu {
     }
 
     public static void main(String[] args) {
-        Customer customer = new Customer();
+        Customer customer = new Customer("dummyUser", "dummyPass");
         CustMenu custMenu = new CustMenu(customer);
 
         custMenu.viewMenu();
