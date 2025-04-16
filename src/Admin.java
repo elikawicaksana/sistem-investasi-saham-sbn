d27bad8b13422d0e94986b4f4c0666a9d2723bcc
Elika <rey048286@gmail.com>
2025-04-16 14:34:53 +0800

    Menyesuaikan class Admin terkait manipulasi data Saham dan SBN dengan class InvestmentData

--- src/Admin.java
+++ src/Admin.java
@@ -3,64 +3,52 @@ package src;
 import java.util.*;
 
 public class Admin extends User {
-    private List<SBN> listSBN;
-    private List<Saham> listSaham;
 
     public Admin(String username, String password) {
         super(username, password);
-        this.listSaham = new ArrayList<>();
-        this.listSBN = new ArrayList<>();
     }
 
-    //  Menampilkan dan menjalankan menu admin setelah admin berhasil login.
-    @Override // Override method dari superclass User
+    // Menampilkan dan menjalankan menu admin setelah login
+    @Override
     public void showMenu() {
         AdminMenu adminMenu = new AdminMenu();
         adminMenu.viewMenu();
     }
 
-    // SBN
+    // Menambahkan produk SBN
     public void addSBN(SBN sbn) {
-        listSBN.add(sbn);
+        InvestmentData.addSBN(sbn);
         System.out.println("SBN " + sbn.getName() + " berhasil ditambahkan!");
     }
 
+    // Menghapus produk SBN
     public void removeSBN(String name) {
-        listSBN.removeIf(sbn -> sbn.getName().equals(name));
-        System.out.println("SBN " + name  + "berhasil dihapus.");
+        InvestmentData.removeSBN(name);
+        System.out.println("SBN " + name + " berhasil dihapus.");
     }
 
-    public List<SBN> getSBNList() {
-        return listSBN;
-    }
-
-    // Saham
+    // Menambahkan produk Saham
     public void addSaham(Saham saham) {
-        listSaham.add(saham);
-        System.out.println("Saham added: " + saham.getCompanyName());
+        InvestmentData.addSaham(saham);
+        System.out.println("Saham " + saham.getCompanyName() + " berhasil ditambahkan!");
     }
 
-    public void removeSaham(String kode){
-        listSaham.removeIf(saham -> saham.getCode().equals(kode));
+    // Menghapus produk Saham
+    public void removeSaham(String kode) {
+        InvestmentData.removeSaham(kode);
+        System.out.println("Saham dengan kode " + kode + " berhasil dihapus.");
     }
 
-    public List<Saham> getSahamList() {
-        return listSaham;
-    }
-
-    // View Product List
+    // Menampilkan seluruh produk investasi
     public void viewInvestmentProducts() {
-        // List Produk SBN
-        System.out.println("SBN List:");
-        for (SBN sbn : listSBN) {
+        System.out.println("=== Daftar Produk SBN ===");
+        for (SBN sbn : InvestmentData.getSBNList()) {
             System.out.println(sbn);
         }
 
-        // List Produk Saham
-        System.out.println("\nStock List:");
-        for (Saham saham : listSaham) {
+        System.out.println("\n=== Daftar Saham ===");
+        for (Saham saham : InvestmentData.getSahamList()) {
             System.out.println(saham);
         }
     }
-
 }
