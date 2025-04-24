18594b808cc8159cfddeda0d06834e58ea4f9afb
Dash <daneshaurus@gmail.com>
2025-04-24 11:54:06 +0800

    Perubahan beberapa kalimat dari bahasa inggris ke bahasa indonesia

--- src/Customer.java
+++ src/Customer.java
@@ -21,12 +21,12 @@ public class Customer extends User {
 
     public void investSaham(Saham saham, int quantity) {
         portofolio.addSaham(saham, quantity);
-        System.out.println("Invested in stock: " + saham.getCompanyName());
+        System.out.println("Berinvestasi pada saham: " + saham.getCompanyName());
     }
 
     public void investSBN(SBN sbn, double amount) {
         portofolio.addSBN(sbn, amount);
-        System.out.println("Invested in SBN: " + sbn.getName());
+        System.out.println("Berinvestasi pada SBN: " + sbn.getName());
     }
     public void viewPortofolio() {
         portofolio.viewPortofolio();
